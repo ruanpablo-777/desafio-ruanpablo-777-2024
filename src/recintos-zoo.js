17f32dcc82805c3f8adccb83f68335db03d4f57f
RuanPablo-7 <ToyaMON777@outlook.com>
2024-09-12 23:20:29 -0300

    update

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,6 +1,6 @@
 class RecintosZoo {
   
-
+    //Tabela dos recintos
     #recintosExistentes = [
         {numero: 1, bioma: "savana", tamanhoGeral: 10, animaisExistentes: "macaco", quantidade: 3},
         {numero: 2, bioma: "floresta", tamanhoGeral: 5, animaisExistentes: "", quantidade: 0},
@@ -8,7 +8,7 @@ class RecintosZoo {
         {numero: 4, bioma: "rio", tamanhoGeral: 8, animaisExistentes: "", quantidade: 0},
         {numero: 5, bioma: "savana", tamanhoGeral: 9, animaisExistentes: "leão", quantidade: 1},
     ]
-
+ // tabela dos animais
     #animais = [
         {especie:"leão", tamanho: 3, bioma:"savana", tipo:"carnivoro"},
         {especie:"leopardo", tamanho: 2, bioma:"savana", tipo:"carnivoro"},
@@ -19,21 +19,24 @@ class RecintosZoo {
     ]
 
     analisaRecintos(animal, quantidade) {
-      
+      // encontra o animal escolhido
         let animalEscolhido = this.#animais.find(a => a.especie === animal)
+        //verifica se o animal existe
         if(!animalEscolhido){
             console.log("animal não encontrado")
             return
         }
-     
+        //verifica se o recinto é adequado para o animal
         let verificarRecinto = this.#recintosExistentes.filter(recinto => {
             return recinto.bioma == animalEscolhido.bioma  
             
         })
+        //se o recinto é adequado diminui o tamanho do espaço
         this.#recintosExistentes.filter(recinto => {
             return recinto.tamanhoGeral = recinto.tamanhoGeral -quantidade 
 
         })
+        // e diminui a quantidade de animais 
         this.#animais.filter(animal => {
             return animal.tamanho -= quantidade
         })
@@ -42,18 +45,18 @@ class RecintosZoo {
         for(let i = 0; i < verificarRecinto.length; i++) {
             if(verificarRecinto.length > 0) {
                 console.log(`RecintoViaveis: [ recinto ${verificarRecinto[i].numero} (espaçolivre: ${verificarRecinto[i].tamanhoGeral - verificarRecinto[i].quantidade} total:${verificarRecinto[i].tamanhoGeral})]`)
+                //imprime o resultado...
             }
             
         }
         
-        console.log(this.#recintosExistentes)
-        console.log(this.#animais)
+        
     }
 
    
 
 }
 
-new RecintosZoo().analisaRecintos("macaco", 1)
+new RecintosZoo().analisaRecintos("crocodilo", 1)
 
-//export { RecintosZoo as RecintosZoo };
+export { RecintosZoo as RecintosZoo };
